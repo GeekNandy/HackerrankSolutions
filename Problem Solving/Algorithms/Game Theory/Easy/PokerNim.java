4db88148019cfda5826714dab0f72788925e009a
Sharad Nanda <5526742+GeekNandy@users.noreply.github.com>
2020-12-30 08:34:25 +0530

    Create PokerNim.java

---
+++ Problem Solving/Algorithms/Game Theory/Easy/PokerNim.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static String pokerNim(int k, int[] c) {
+        int s = 0;
+        for(int i: c) s^=i;
+        if(s!=0) return "First";
+        else return "Second";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] nk = scanner.nextLine().split(" ");
+
+            int n = Integer.parseInt(nk[0]);
+
+            int k = Integer.parseInt(nk[1]);
+
+            int[] c = new int[n];
+
+            String[] cItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int cItem = Integer.parseInt(cItems[i]);
+                c[i] = cItem;
+            }
+
+            String result = pokerNim(k, c);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
